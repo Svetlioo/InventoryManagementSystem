2737ba1264ec70a477d4bb221bada6841429e49a
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-10-30 21:55:57 +0200

    Add helper method to exit to main menu

--- src/main/java/com/system/helperMethods/Methods.java
+++ src/main/java/com/system/helperMethods/Methods.java
@@ -2,11 +2,12 @@ package com.system.helperMethods;
 
 import com.system.itemTypes.InventoryItem;
 
+import java.util.Scanner;
+
 public class Methods {
     public static void showLoginChoices() {
-        System.out.println("""
-                Press 1 for Login
-                Press 2 for Register""");
+        System.out.println("Press 1 for Login");
+        System.out.println("Press 2 for Register");
     }
 
     public static void showWelcomeMessage(String username) {
@@ -17,6 +18,23 @@ public class Methods {
         System.out.println("| " + welcomeMessage + " |");
         System.out.println(border);
 
+    }
+
+    public static void displayTestAccount() {
+        System.out.println("Test account");
+        System.out.println("username: test");
+        System.out.println("password: 123");
+        System.out.println();
+    }
+
+    public static void exitInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type \"exit\" to go back to main menu!");
+        String toExit = scanner.nextLine();
+        while (!toExit.equalsIgnoreCase("exit")) {
+            System.out.println("Type \"exit\" to go back to main menu!");
+            toExit = scanner.nextLine();
+        }
     }
 
 }
